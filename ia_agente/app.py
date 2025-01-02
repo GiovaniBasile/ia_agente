d7ba0782b69ff51c183000d4040051090e072a65
Giovani Basile <giovanibasile@gmail.com>
2025-01-02 17:30:53 -0300

    1. Adicionando verificação de estado de retorno. 2. Adicionando camada de validação de contrato.

--- ia_agente/app.py
+++ ia_agente/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from ia_agente.schemas import Mensagem
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Mensagem)
 def read_root():
     return {'message': 'Olá mundo!'}
